5d7054db76fdac5cb613cfc3122128c5896c24d8
Naiara <naiara@mentorspace.dev>
2022-10-11 16:23:29 -0300

    Adicionando responsividade em listagem

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,10 +1,16 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Th, Thead, Tr, Text } from "@chakra-ui/react";
+import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Th, Thead, Tr, Text, useBreakpointValue } from "@chakra-ui/react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
+
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList() {
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true,
+  })
+
   return (
     <Box>
       <Header />
@@ -24,17 +30,18 @@ export default function UserList() {
           <Table colorScheme="whiteAlpha">
             <Thead>
               <Tr>
-                <Th px="6" color="gray.300" width="8">
+                <Th px={["4", "4", "6"]} color="gray.300" width="8">
                   <Checkbox colorScheme="pink" />
                 </Th>
                 <Th>Usuário</Th>
-                <Th>Data de cadastro</Th>
+                {  isWideVersion && <Th>Data de cadastro</Th> }
+                
                 <Th width="8"></Th>
               </Tr>
             </Thead>
             <Tbody>
               <Tr>
-               <Td px="6">
+               <Td px={["4", "4", "6"]}>
                <Checkbox colorScheme="pink" />
                </Td>
                <Td>
@@ -43,7 +50,7 @@ export default function UserList() {
                   <Text fontSize="sm" color="gray.300">naaaidev@gmail.com</Text>
                 </Box>
                </Td>
-               <Td>20 de Setembro, 2022</Td>
+               { isWideVersion && <Td>20 de Setembro, 2022</Td>}
                <Td>
                <Button as="a" size="sm" fontSize="sm" colorScheme="purple" leftIcon={<Icon as={RiPencilLine} fontSize="16" />}>
               Editar
@@ -52,7 +59,7 @@ export default function UserList() {
               </Tr>
 
               <Tr>
-               <Td px="6">
+               <Td px={["4", "4", "6"]}>
                <Checkbox colorScheme="pink" />
                </Td>
                <Td>
@@ -61,7 +68,7 @@ export default function UserList() {
                   <Text fontSize="sm" color="gray.300">naaaidev@gmail.com</Text>
                 </Box>
                </Td>
-               <Td>20 de Setembro, 2022</Td>
+               { isWideVersion && <Td>20 de Setembro, 2022</Td>}
                <Td>
                <Button as="a" size="sm" fontSize="sm" colorScheme="purple" leftIcon={<Icon as={RiPencilLine} fontSize="16" />}>
               Editar
@@ -70,7 +77,7 @@ export default function UserList() {
               </Tr>
 
               <Tr>
-               <Td px="6">
+               <Td px={["4", "4", "6"]}>
                <Checkbox colorScheme="pink" />
                </Td>
                <Td>
@@ -79,7 +86,7 @@ export default function UserList() {
                   <Text fontSize="sm" color="gray.300">naaaidev@gmail.com</Text>
                 </Box>
                </Td>
-               <Td>20 de Setembro, 2022</Td>
+               { isWideVersion && <Td>20 de Setembro, 2022</Td>}
                <Td>
                <Button as="a" size="sm" fontSize="sm" colorScheme="purple" leftIcon={<Icon as={RiPencilLine} fontSize="16" />}>
               Editar
