5f69f57e24df942e172dee0e584c673b18f6bc04
Naiara <naiara@mentorspace.dev>
2022-10-11 23:13:07 -0300

    Criando formularios de autenticacao

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -4,16 +4,18 @@ import {
   Input as ChakraInput,
   InputProps as ChakraInputProps,
 } from "@chakra-ui/react";
+import { forwardRef, ForwardRefRenderFunction } from "react";
 
 interface InputProps extends ChakraInputProps {
   name: string;
   label?: string;
 }
 
-export function Input({ name, label, ...rest }: InputProps) {
+const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> = ({ name, label, ...rest }, ref) => {
   return (
     <FormControl>
-      {!!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
+         {!!label && <FormLabel htmlFor={name} id={`label-${name}`}>{label} 
+      </FormLabel>}
 
       <ChakraInput
         name={name}
@@ -23,8 +25,11 @@ export function Input({ name, label, ...rest }: InputProps) {
         variant="filled"
         _hover={{ bgColor: "gray.900" }}
         size="lg"
+        ref={ref}
         {...rest}
       />
     </FormControl>
   );
 }
+
+export const Input = forwardRef(InputBase);
